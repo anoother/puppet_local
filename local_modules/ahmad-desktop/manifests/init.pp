78ab435ae86503fa024a77bb8cc83b660605668c
Ahmad Jagot <ahmadjagot@gmail.com>
2016-12-24 17:43:57 +0000

    Add apt repos

--- local_modules/ahmad-desktop/manifests/init.pp
+++ local_modules/ahmad-desktop/manifests/init.pp
@@ -13,6 +13,50 @@ class desktop {
     ]:
   }
 
+  apt::source { 'steam':
+    location     => 'http://repo.steampowered.com/steam/',
+    repos        => 'steam',
+    release      => 'precise',
+    architecture => 'amd64,i386',
+    include      => {
+      'deb' => true,
+      'src' => true,
+    },
+    key          => {
+      'id' => 'BA1816EF8E75005FCF5E27A1F24AEA9FB05498B7',
+    },
+  }
+
+  apt::source { 'nodesource':
+    location => 'https://deb.nodesource.com/node_5.x/',
+    repos    => 'main',
+    release  => 'xenial',
+    include  => {
+      'deb' => true,
+      'src' => true,
+    },
+    key      => {
+      'id' => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
+    },
+  }
+
+  define google_apt_source() {
+    apt::source { $name:
+      location     => "http://dl.google.com/linux/${name}/deb/",
+      repos        => 'main',
+      release      => 'stable',
+      architecture => 'amd64',
+      include      => {
+        'deb' => true,
+        'src' => false,
+      },
+      key          => {
+        'id' => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
+      },
+    }
+  }
+  google_apt_source { ['chrome', 'chrome-remote-desktop', ]: }
+
   package {
     [
       'xmonad',
@@ -30,6 +74,8 @@ class desktop {
       'texlive-fonts-recommended', # For pandoc
       'wkhtmltopdf', # For pandoc
       'lm-sensors',
+      'puppet-lint',
+      'steam',
     ]: ensure => present
   }
 
