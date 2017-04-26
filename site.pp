360f4f6fee06768babfc883ecb8e2b30a5c49fbc
Ahmad Jagot <ahmadjagot@gmail.com>
2017-04-26 19:31:53 +0100

    Add deskside

--- site.pp
+++ site.pp
@@ -1,3 +1,11 @@
-include desktop
-include multigpu
-include opal
+node deskside {
+  include multigpu
+  include nvidia
+}
+
+node shadow {
+  include desktop
+  include multigpu
+  include nvidia
+  include opal
+}
