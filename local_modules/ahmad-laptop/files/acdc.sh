943faab98d8ca22871e09b5dd7bb421448fb1b86
Ahmad Jagot <ahmadjagot@gmail.com>
2017-07-07 15:32:41 +0100

    Set cpufreq governor in response to charger events.

--- local_modules/ahmad-laptop/files/acdc.sh
+++ local_modules/ahmad-laptop/files/acdc.sh
@@ -12,17 +12,22 @@ unplug_p='min_power'
 plugin_a='on'
 unplug_a='auto'
 
+plugin_c='performance'
+unplug_c='powersave'
+
 function main() {
     case $1 in
         plugin|unplug)
             eval n=\$${1}_n
             eval p=\$${1}_p
             eval a=\$${1}_a
+            eval c=\$${1}_c
 
             echo $p | tee /sys/class/scsi_host/host*/link_power_management_policy
             echo $a | tee /sys/bus/pci/devices/*/power/control
             echo $a > /sys/bus/i2c/devices/i2c-1/device/power/control
             echo $b > /sys/module/snd_hda_intel/parameters/power_save
+            cpupower frequency-set -g $c
             ;;
         *)
             echo "Usage: $0 {plugin|unplug}"
