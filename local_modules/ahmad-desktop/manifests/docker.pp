a78dae841866ac00e4d945608d0e7ee4711f25eb
Ahmad Jagot <ahmadjagot@gmail.com>
2017-10-07 14:23:15 +0100

    Add docker.

---
+++ local_modules/ahmad-desktop/manifests/docker.pp
@@ -0,0 +1,30 @@
+class desktop::docker {
+
+  apt::source { 'docker':
+    location     => 'https://download.docker.com/linux/ubuntu',
+    repos        => 'stable',
+    release      => 'xenial',
+    architecture => 'amd64',
+    include      => {
+      'deb' => true,
+      'src' => false,
+    },
+    key          => {
+      'id'     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+      'source' => 'https://download.docker.com/linux/ubuntu/gpg',
+    }
+  } ->
+
+  apt::pin { 'docker':
+    originator => "Docker",
+    priority   => 501,
+  } ->
+
+  package {[
+    'docker-ce',
+  ]:
+    ensure  => present,
+    require => Exec['apt_update'],
+  }
+
+}
--- local_modules/ahmad-desktop/manifests/init.pp
+++ local_modules/ahmad-desktop/manifests/init.pp
@@ -3,6 +3,7 @@ class desktop {
   require apt
   require udev
 
+  include desktop::docker
   include desktop::fix_ubuntu
   include desktop::fonts
   include desktop::gaming
