9046b729add5d05eb5a342b53d0bad9da9305202
Ahmad Jagot <ahmadjagot@gmail.com>
2017-12-18 01:15:47 +0000

    compute-firmware package provides firmware

--- local_modules/ahmad-vega/manifests/init.pp
+++ local_modules/ahmad-vega/manifests/init.pp
@@ -2,27 +2,6 @@ class vega {
 
 require apt
 
-  $firmware_source = "https://people.freedesktop.org/~agd5f/radeon_ucode/vega10"
-  wget::fetch { [
-    "${firmware_source}/vega10_acg_smc.bin",
-    "${firmware_source}/vega10_asd.bin",
-    "${firmware_source}/vega10_ce.bin",
-    "${firmware_source}/vega10_gpu_info.bin",
-    "${firmware_source}/vega10_me.bin",
-    "${firmware_source}/vega10_mec.bin",
-    "${firmware_source}/vega10_mec2.bin",
-    "${firmware_source}/vega10_pfp.bin",
-    "${firmware_source}/vega10_rlc.bin",
-    "${firmware_source}/vega10_sdma.bin",
-    "${firmware_source}/vega10_sdma1.bin",
-    "${firmware_source}/vega10_smc.bin",
-    "${firmware_source}/vega10_sos.bin",
-    "${firmware_source}/vega10_uvd.bin",
-    "${firmware_source}/vega10_vce.bin",
-  ]:
-      destination => '/lib/firmware/amdgpu/',
-  }
-
   apt::ppa { 'ppa:paulo-miguel-dias/mesa':
     ensure => absent,
     before =>  Exec['apt_update'],
@@ -57,12 +36,12 @@ require apt
       'source' => 'http://repo.radeon.com/rocm/apt/debian/rocm.gpg.key',
     },
       before =>  Exec['apt_update'],
-  } #->
+  } ->
 
-  #package { [
-  #  'rocm-opencl',
-  #]:
-  #  ensure  => present,
-  #}
+  package { [
+    'compute-firmware',
+  ]:
+    ensure  => present,
+  }
 
 }
